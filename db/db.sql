b7d9789f579ff6108b9bd72225235671387b4512
ghizlane <ghlaneiz022@gmail.com>
2024-03-26 23:03:52 +0000

    Update db.sql

--- db/db.sql
+++ db/db.sql
@@ -10,13 +10,17 @@ create table clients(
     nom varchar(50) not null,
     prenom varchar(50) not null,
     email varchar(50) not null,
-    password_ varchar(15) not null
+    password_ varchar(15) not null,
+    addresse varchar(255) not null,
+    phone varchar(15) not null,
+    compte_status varchar(30) not null
 );
 
 -- table structure for table `services`
 create table services(
     service_id int primary key auto_increment,
-    nom varchar(255) not null
+    nom varchar(255) not null,
+    service_status varchar(30) not null
 );
 
 -- Table structure for table `experts`
@@ -28,7 +32,7 @@ create table experts(
     password_ varchar(15) not null,
     photo varchar(70),
     metier varchar(100) not null,
-    ville varchar(100) not null
+    compte_status varchar(30) not null
 );
 
 -- Table structure for table `service_expert` (many to many relationship)
@@ -40,6 +44,8 @@ create table service_expert(
     disponibilite varchar(255),
     duree varchar(100),
     prix_par_duree float,
+    ville varchar(100) not null,
+    status_ varchar(30),
     foreign key (expert_id) references experts(expert_id),
     foreign key (service_id) references services(service_id),
     primary key (expert_id, service_id)
@@ -55,29 +61,6 @@ create table admins(
 );
 
 
--- Table structure for table `commentaires_sur_clients`
-create table commentaires_sur_clients(
-    commentaire_id int primary key auto_increment,
-    note int not null,
-    client_id int not null,
-    expert_id int not null,
-    commentaire varchar(255) not null,
-    foreign key (client_id) references clients(client_id),
-    foreign key (expert_id) references experts(expert_id)
-);
-
-create table commentaires_sur_experts(
-    commentaire_id int primary key auto_increment,
-    note int not null,
-    client_id int not null,
-    expert_id int not null,
-    service_id int not null,
-    commentaire varchar(255) not null,
-    foreign key (client_id) references clients(client_id),
-    foreign key (expert_id) references experts(expert_id),
-    foreign key (service_id) references services(service_id)
-);
-
 -- Table structure for table `demandes_clients`
 create table demandes_clients(
     demande_id int primary key auto_increment,
@@ -92,4 +75,32 @@ create table demandes_clients(
     foreign key (client_id) references clients(client_id),
     foreign key (expert_id) references experts(expert_id),
     foreign key (service_id) references services(service_id)
+);
+
+
+-- Table structure for table `commentaires_sur_clients`
+create table commentaires_sur_clients(
+    commentaire_id int primary key auto_increment,
+    note int not null,
+    demande_id int not null,
+    commentaire varchar(255) not null,
+    foreign key (demande_id) references demandes_clients(demande_id)
+);
+
+
+create table commentaires_sur_experts(
+    commentaire_id int primary key auto_increment,
+    note int not null,
+    demande_id int not null,
+    commentaire varchar(255) not null,
+    foreign key (demande_id) references demandes_clients(demande_id)
+);
+
+create table contact(
+    contact_id int primary key auto_increment,
+    nom varchar(50) not null,
+    prenom varchar(50) not null,
+    phone varchar(15) not null,
+    email varchar(50) not null,
+    message_ varchar(255) not null
 );
