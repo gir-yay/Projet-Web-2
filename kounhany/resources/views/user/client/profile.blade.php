c1dd8ff5136359ed25a97662da9b51e65b1887c7
ghizlane <ghlaneiz022@gmail.com>
2024-04-19 10:54:07 +0100

    profile UI

--- kounhany/resources/views/user/client/profile.blade.php
+++ kounhany/resources/views/user/client/profile.blade.php
@@ -4,7 +4,7 @@
 <meta charset="utf-8">
 
 
-<title>about me - Bootdey.com</title>
+<title>Client Profile</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="{{ asset('css/profile.css') }}">
